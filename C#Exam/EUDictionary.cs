245f4eaff45da02dbba70c1a8c6b5b0151ce058f
dgmnkk <gumenyukdiana@gmail.com>
2023-05-02 17:06:56 +0300

    Добавила основні методи для функціоналу класу

---
+++ C#Exam/EUDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Exam
+{
+    internal class EUDictionary
+    {
+        public Dictionary<string, object> dict;
+        public EUDictionary()
+        {
+            dict = new Dictionary<string, object>();
+        }
+        public EUDictionary(Dictionary<string, object> dict)
+        {
+            this.dict = dict;
+        }
+        public void AddNewWord(string word, object traslate)
+        {
+            dict.Add(word, traslate);
+        }
+        public void RemoveWord(string word)
+        {
+            dict.Remove(word);
+        }
+        public void ChangeTranslate(string word)
+        {
+            Console.WriteLine("Enter new translate: ");
+            string tmpTranslate = Console.ReadLine();
+            dict[word] = tmpTranslate;
+        }
+        public void Traslate(string word)
+        {
+            dict.TryGetValue(word, out var traslate);
+            Console.WriteLine(word + "-"+ traslate);
+        }
+    }
+}
--- C#Exam/Program.cs
+++ C#Exam/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace C_Exam
+﻿using System.Text;
+
+namespace C_Exam
 {
+    
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            //int choise = -1;
+            //do
+            //{
+            //    Console.WriteLine("_____English-Ukrainian Dictionary_____");
+            //    Console.WriteLine("1. Add new word\n2. Change word\n3. Delete word\n4. Translate\n5. Download dictionary from file\n6. Save dictionary in file\n0. Exit");
+            //    choise = Convert.ToInt32(Console.ReadLine());
+            //    switch( choise )
+            //    {
+            //        case 1:
+            //            Console.Clear();
+            //            Console.WriteLine("Add new word to dictionary");
+            //            break;
+            //    }
+
+            //} while (choise != 0);
+
+            EUDictionary d = new EUDictionary();
+            d.AddNewWord("Hello", "Привіт");
+            d.Traslate("Hello");
+            d.ChangeTranslate("Hello");
+            d.Traslate("Hello");
         }
     }
 }
