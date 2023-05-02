eb8b498946a4de3bac84e31320852f3ab3e7e30a
dgmnkk <gumenyukdiana@gmail.com>
2023-05-02 20:18:38 +0300

    Зроблено все, окрім множинного перекладу

--- C#Exam/EUDictionary.cs
+++ C#Exam/EUDictionary.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+
 
 namespace C_Exam
 {
     internal class EUDictionary
     {
-        public Dictionary<string, object> dict;
+        public Dictionary<string, string> dict;
+        
         public EUDictionary()
         {
-            dict = new Dictionary<string, object>();
+            dict = new Dictionary<string, string>();
         }
-        public EUDictionary(Dictionary<string, object> dict)
+        public EUDictionary(Dictionary<string, string> dict)
         {
             this.dict = dict;
         }
-        public void AddNewWord(string word, object traslate)
+        public void AddNewWord(string word, string traslate)
         {
             dict.Add(word, traslate);
         }
@@ -36,5 +39,33 @@ namespace C_Exam
             dict.TryGetValue(word, out var traslate);
             Console.WriteLine(word + "-"+ traslate);
         }
+        public void Print()
+        {
+            var keys = dict.Keys;
+            foreach (string key in keys)
+            {
+                Console.WriteLine(key + "-" + dict[key]);
+            }
+        } 
+        public void SaveInFile()
+        {
+            string FileName = "English-UkrainianDictionary.json";
+            try
+            {
+                var options = new JsonSerializerOptions 
+                { 
+                    WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All), 
+                };
+                string SerializedList = JsonSerializer.Serialize(dict, options);
+                
+                File.WriteAllText(FileName, SerializedList);
+
+                Console.WriteLine($"Done! Look at file {FileName}");
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message); }
+
+        }
+
     }
 }
--- C#Exam/Program.cs
+++ C#Exam/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Text.Json;
 
 namespace C_Exam
 {
@@ -7,28 +9,75 @@ namespace C_Exam
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = UTF8Encoding.UTF8;
-            //int choise = -1;
-            //do
-            //{
-            //    Console.WriteLine("_____English-Ukrainian Dictionary_____");
-            //    Console.WriteLine("1. Add new word\n2. Change word\n3. Delete word\n4. Translate\n5. Download dictionary from file\n6. Save dictionary in file\n0. Exit");
-            //    choise = Convert.ToInt32(Console.ReadLine());
-            //    switch( choise )
-            //    {
-            //        case 1:
-            //            Console.Clear();
-            //            Console.WriteLine("Add new word to dictionary");
-            //            break;
-            //    }
+            int choise = -1;
+            int c = -1;
+            string FileName = "English-UkrainianDictionary.json";
+            Console.WriteLine("_____English-Ukrainian Dictionary_____");
+            Console.WriteLine("1. Create new dictionary 2. Download from file");
+            c = Convert.ToInt32(Console.ReadLine());
+            EUDictionary d= new EUDictionary();
+            switch (c)
+            {
+                case 1:
+                    d = new EUDictionary();
+                    break;
+                case 2:
+                    string deserializedList = File.ReadAllText(FileName);
+                    Dictionary<string, string> tmp = JsonSerializer.Deserialize<Dictionary<string, string>>(deserializedList);
+                    d = new EUDictionary(tmp);
+                    break;
+            }
+            do
+            {
+                
+                Console.WriteLine("1. Add new word\n2. Change word\n3. Delete word\n4. Translate\n5. Save dictionary in file\n6. Print dictionary\n0. Exit");
+                choise = Convert.ToInt32(Console.ReadLine());
+                string w;
+                string t;
+                
+                switch (choise)
+                {
+                    case 1:
+                        
+                        Console.WriteLine("Add new word to dictionary");
+                        Console.WriteLine("Enter word in English: ");
+                        w = Console.ReadLine();
+                        Console.WriteLine("Enter translate: ");
+                        t = Console.ReadLine();
+                        d.AddNewWord(w, t);
+                        break;
+                    case 2:
+                        
+                        Console.WriteLine("Change word");
+                        Console.WriteLine("Enter word which translate you want to change: ");
+                        w = Console.ReadLine();
+                        d.ChangeTranslate(w);
+                        break; 
+                    case 3:
+                        
+                        Console.WriteLine("Enter word you want to delete: ");
+                        w = Console.ReadLine();
+                        d.RemoveWord(w);
+                        break;
+                    case 4:
+                        
+                        Console.WriteLine("Enter word you want to find translate of: ");
+                        w = Console.ReadLine();
+                        d.Traslate(w);
+                        break;
+                    case 5:
+                        
+                        d.SaveInFile();
+                        break;
+                    case 6:
+                        
+                        Console.WriteLine("____Dictionary_____");
+                        d.Print();
+                        break;
+                    default: Console.WriteLine("Try again"); break;
+                }
 
-            //} while (choise != 0);
-
-            EUDictionary d = new EUDictionary();
-            d.AddNewWord("Hello", "Привіт");
-            d.Traslate("Hello");
-            d.ChangeTranslate("Hello");
-            d.Traslate("Hello");
+            } while (choise != 0);
         }
     }
 }
